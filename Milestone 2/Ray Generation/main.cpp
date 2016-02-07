1074e97fb1daebeffa4918fca8c24dd458095973
Stuart Bowman <stuartsoft2012@gmail.com>
2016-02-06 19:38:53 -0500

    Actually added starter code from Boatright

--- Milestone 2/Ray Generation/main.cpp
+++ Milestone 2/Ray Generation/main.cpp
@@ -1,3 +1,53 @@
-int main(int argc, char* argv[]){
+/**
+ * An example program that creates a 24-bit bitmap file that is 800 x 600 and makes a blue/red checkerboard pattern.
+ * Uses EasyBMP
+ *
+ * Cory Boatright
+ * University of Pennsylvania, Fall 2011
+ * AND Grove City College 2015
+ **/
+
+#include "EasyBMP.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	unsigned int width = 800;
+	unsigned int height = 600;
+
+	BMP output;
+	output.SetSize(width, height);
+	output.SetBitDepth(24);
+	
+	for(unsigned int x = 0; x < width; x++) {
+		for(unsigned int y = 0; y < height; y++) {
+			if(x % 2 == 0) {
+				if(y % 2 == 0) {
+					output(x, y)->Red = 255;
+					output(x, y)->Blue = 0;
+					output(x, y)->Green = 0;
+				}
+				else {
+					output(x, y)->Red = 0;
+					output(x, y)->Blue = 255;
+					output(x, y)->Green = 0;
+				}
+			}
+			else {
+				if(y % 2 == 0) {
+					output(x, y)->Red = 0;
+					output(x, y)->Blue = 255;
+					output(x, y)->Green = 0;
+				}
+				else {
+					output(x, y)->Red = 255;
+					output(x, y)->Blue = 0;
+					output(x, y)->Green = 0;
+				}
+			}
+		}
+	}
+
+	output.WriteToFile("output.bmp");
 	return 0;
 }
