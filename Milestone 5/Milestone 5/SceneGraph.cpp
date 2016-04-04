323eb446de52a5c5755ee245a064d2842b03507a
Stuart Bowman <stuartsoft2012@gmail.com>
2016-04-04 01:18:05 -0400

    Not working yet, but much progress has been made

--- Milestone 5/Milestone 5/SceneGraph.cpp
+++ Milestone 5/Milestone 5/SceneGraph.cpp
@@ -1,5 +1,13 @@
 #include "SceneGraph.h"
 
+SceneGraph::SceneGraph(){
+	geometry = NULL;
+	translation = glm::mat4();
+	scale = glm::mat4();
+	rotation = glm::mat4();
+	parentSG = NULL;
+}
+
 void SceneGraph::Parse(string fname){
 	ifstream file(fname);
 
@@ -14,6 +22,13 @@ void SceneGraph::Parse(string fname){
 	zSize = stoi(tok);
 	ss >> tok;
 	int numItems = stoi(tok);
+
+	//initialize the root node's TRS
+	translation = glm::mat4();
+	glm::translate(translation,glm::vec3(xSize/2, 0, zSize/2));//translate the root to the center
+	scale = glm::mat4();
+	rotation = glm::mat4();
+	rootSG = this;
 
 	//Build the scene graph nodes as we read the file
 
@@ -40,9 +55,37 @@ void SceneGraph::Parse(string fname){
 		scale.y = stoi(tok);
 		ss >> tok;
 		scale.z = stoi(tok);
+		
+		//figure out where this should go in the scene graph
+		//first check if any objects exist in this pos, if there are, stack this one on top
+		float yIndex = 0;
+		bool check = true;
+		glm::vec3 targetPos = glm::vec3(xIndex, yIndex, zIndex);
+		while (check)
+		{
+			SceneGraph* tempSG = checkIfObjectExistsHere(glm::vec3(xIndex,0,zIndex));
+			yIndex = tempSG.
+		}
 	}
 
 	file.close();
-
 }
 
+SceneGraph* SceneGraph::checkIfObjectExistsHere(glm::vec3 pos){
+	for(int i = 0;i<decendents.size;i++){
+		SceneGraph* temp = decendents[i].checkIfObjectExistsHere(pos);
+	}
+}
+
+void SceneGraph::addChild(SceneGraph child){
+	child.rootSG = rootSG;
+	child.parentSG = this;
+	decendents.push_back(child);
+}
+
+void SceneGraph::traverse(glm::mat4 mat){
+
+	for (int i = 0;i<decendents.size;i++){
+		decendents[i].traverse(mat);
+	}
+}
--- Milestone 5/Milestone 5/SceneGraph.h
+++ Milestone 5/Milestone 5/SceneGraph.h
@@ -3,8 +3,12 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include "glm\glm.hpp"
+#include "glm\gtc\matrix_transform.hpp"
+
+#include "Geometry.h"
 
 using namespace std;
 
@@ -12,6 +16,20 @@ class SceneGraph {
 public:
 	SceneGraph(){};
 	void Parse(string fname);
+	SceneGraph* checkIfObjectExistsHere(glm::vec3 pos);
+
+	glm::mat4 translation;
+	glm::mat4 rotation;
+	glm::mat4 scale;
+
+	SceneGraph* rootSG;//pointer back to Scene Graph root node
+	SceneGraph* parentSG;//pointer to parent scene graph
+	vector<SceneGraph> decendents; //vector of all children of this scene graph
+
+	Geometry* geometry;
+
+	void traverse(glm::mat4 mat);
+	void addChild(SceneGraph child);
 
 private:
 	int xSize;
