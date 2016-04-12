7ac05d35668e9d7c7dd435e7d3c9b21eb2eb9c2a
Stuart Bowman <stuartsoft2012@gmail.com>
2016-04-11 23:46:21 -0400

    Changed camera config. Added screenshot

--- Project 2/ShaderTestFramework/my_openglwidget.cpp
+++ Project 2/ShaderTestFramework/my_openglwidget.cpp
@@ -129,7 +129,7 @@ void My_OpenGLWidget::paintGL() {
 }
 
 void My_OpenGLWidget::createCamera() {
-	camLocation = vec4(0.0f, 1.0f, 2.5f, 1.0f);
+	camLocation = vec4(0.0f, 0.0f, 2.0f, 1.0f);
 	up = vec4(0.0f, 1.0f, 0.0f, 0.0f);
 	focus = vec4(0.0, 0.0, 0.0, 1.0f);
 
