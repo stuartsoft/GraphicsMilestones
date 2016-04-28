4ae3c6e85b0e9638a9bb1a6bdf557693e55b6de3
Stuart Bowman <stuartsoft2012@gmail.com>
2016-04-27 22:22:36 -0400

    Added rayGeneration() function and constructor

--- Milestone 7/Milestone 7/RayTracer.cpp
+++ Milestone 7/Milestone 7/RayTracer.cpp
@@ -1,1 +1,40 @@
 #include "RayTracer.h"
+
+const vec3 BACKGROUND_COLOR = vec3(255, 255, 255);
+#define PI 3.14159
+
+void RayTracer::rayGeneration(int wid, int height){
+	
+	vec3 N = vdir;
+	N = normalize(N);
+
+	vec3 up = uvec;
+	up = normalize(up);
+	vec3 U;
+
+	vec3 m = eyePos + N;
+
+	U = glm::cross(N,up);
+	U = normalize(U);
+
+	vec3 V, H;
+	float tanFovy = tan(fovy * (PI / 180.0f));
+	V = up * tanFovy;
+
+	H = U * tanFovy;
+
+	for (unsigned int x = 0;x<wid;x++){
+		for (unsigned int y = 0;y<height;y++){
+			glm::vec3 D;
+			float xPercent = (2.0f * x/(wid-1)-1);
+			float yPercent = (2.0f * y/(height-1)-1);
+			D = m + (H * xPercent) + (V * yPercent);
+			glm::vec3 R = D - eyePos;
+			R = glm::normalize(R);
+
+			//TODO call intersection tests on all objects and call shadow feelers
+
+		}
+	}
+
+}
--- Milestone 7/Milestone 7/RayTracer.h
+++ Milestone 7/Milestone 7/RayTracer.h
@@ -1,17 +1,29 @@
 #include "stubs.h"
+#include "glm\glm.hpp"
 //#include "Geometry.h"
 
 class RayTracer
 {
 public:
-
+	RayTracer(vec3 EyePos, vec3 Vdir, vec3 Uvec, float Fovy){
+		eyePos = EyePos;
+		vdir = Vdir;
+		uvec = Uvec;
+		fovy = Fovy;
+	}
 
 	//Calculate all lighting along with the shadow feelers affecting that lighting
 	void shadowFeeler();
 
 	//Generate the rays starting from the camera and going to a point on the screen
-	void rayGeneration();
+	void RayTracer::rayGeneration(int wid, int height);
 
 
 private:
+
+	glm::vec3 eyePos;
+	glm::vec3 vdir;
+	glm::vec3 uvec;
+	float fovy;
+
 };
