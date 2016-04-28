4d4476be0318de30311be09f7d31d0043d1f0cce
unknown <PORTERDJ1@2CE32811DY.GCC.edu>
2016-04-27 23:19:34 -0400

    Making changes along the way

--- Milestone 7/Milestone 7/RayTracer.cpp
+++ Milestone 7/Milestone 7/RayTracer.cpp
@@ -3,7 +3,7 @@
 const vec3 BACKGROUND_COLOR = vec3(255, 255, 255);
 #define PI 3.14159
 
-void RayTracer::rayGeneration(int wid, int height){
+void RayTracer::rayGeneration(const mat4& transMatrix){
 	
 	vec3 N = vdir;
 	N = normalize(N);
@@ -23,18 +23,45 @@ void RayTracer::rayGeneration(int wid, int height){
 
 	H = U * tanFovy;
 
-	for (unsigned int x = 0;x<wid;x++){
-		for (unsigned int y = 0;y<height;y++){
+	BMP output;
+	output.SetSize(imageSize.x, imageSize.y);
+	output.SetBitDepth(24);
+
+	for (unsigned int x = 0;x<imageSize.x;x++){
+		for (unsigned int y = 0;y<imageSize.y;y++){
 			glm::vec3 D;
-			float xPercent = (2.0f * x/(wid-1)-1);
-			float yPercent = (2.0f * y/(height-1)-1);
+			float xPercent = (2.0f * x/(imageSize.x-1)-1);
+			float yPercent = (2.0f * y/(imageSize.y-1)-1);
 			D = m + (H * xPercent) + (V * yPercent);
 			glm::vec3 R = D - eyePos;
 			R = glm::normalize(R);
 
 			//TODO call intersection tests on all objects and call shadow feelers
+			double t = 1e26;
+			for(unsigned num=0; num < geomList.size(); ++num)
+			{
+				double tOne = intersectionTests(geomList[num], eyePos, R, transMatrix);
 
+				if(tOne < t)
+					t = tOne;
+			}
+			vec3 color = BACKGROUND_COLOR;
+			if(t != -1)
+			{
+				double sFeeler = shadowFeeler(t);
+				color = calculateLighting(sFeeler);
+			}
+
+			output(x, y)->Red = abs(R.x) * 255;
+			output(x, y)->Green = abs(R.y) * 255;
+			output(x, y)->Blue = abs(R.z) * 255;
 		}
 	}
+
+	output.WriteToFile("Output.bmp");
+}
+
+vec3 RayTracer::calculateLighting(double sFeeler)
+{
 
 }
--- Milestone 7/Milestone 7/RayTracer.h
+++ Milestone 7/Milestone 7/RayTracer.h
@@ -1,26 +1,32 @@
 #include "stubs.h"
 #include "glm\glm.hpp"
-//#include "Geometry.h"
+#include "Geometry.h"
+#include "EasyBMP.h"
 
 class RayTracer
 {
 public:
-	RayTracer(vec3 EyePos, vec3 Vdir, vec3 Uvec, float Fovy){
+	RayTracer(vec3 EyePos, vec3 Vdir, vec3 Uvec, float Fovy, vec2 picSize, std::vector<Geometry*> list){
 		eyePos = EyePos;
 		vdir = Vdir;
 		uvec = Uvec;
 		fovy = Fovy;
+		imageSize = picSize;
+		geomList = list;
 	}
 
 	//Calculate all lighting along with the shadow feelers affecting that lighting
-	void shadowFeeler();
+	double shadowFeeler(vec4 intersectPoint);
 
 	//Generate the rays starting from the camera and going to a point on the screen
-	void RayTracer::rayGeneration(int wid, int height);
+	void rayGeneration(const mat4& transMatrix);
 
+	//Calculate the lighting for the pixel
+	vec3 calculateLighting(double shadowFeeler);
 
 private:
-
+	std::vector<Geometry*> geomList;
+	glm::vec2 imageSize;
 	glm::vec3 eyePos;
 	glm::vec3 vdir;
 	glm::vec3 uvec;
