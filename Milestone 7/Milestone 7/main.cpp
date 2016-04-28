85d76be64333a41586148cf65b2efc091e8c8b31
Stuart Bowman <stuartsoft2012@gmail.com>
2016-04-27 23:46:43 -0400

    Did more stuff on the driver. All scene parameters are now setup and ready to use

--- Milestone 7/Milestone 7/main.cpp
+++ Milestone 7/Milestone 7/main.cpp
@@ -12,18 +12,36 @@ return 0;
 
 
 void runScenes(){
-	float rotY = 0;
-	glm::vec3 scale = glm::vec3(1.5, 1, 1);
-	glm::vec3 pos = glm::vec3(1, 0, 0);//used for x,z position
-
-	std::vector<Geometry *> geos;
+	std::vector<Geometry*> geos;
 	geos.push_back(new Sphere());
 	geos.push_back(new Cube());
 	geos.push_back(new Triangle());
-		
+
+	std::vector<glm::vec4> camPos;
+	camPos.push_back(glm::vec4(0, 0, 3, 0));
+	camPos.push_back(glm::vec4(-3, 0, 0, 0));
+	camPos.push_back(glm::vec4(1, 1, 1, 0));
+	
+
 	for (int i = 0;i<3;i++){//for each type of shape
-		//create Ray Tracer
-		
+		for (int j = 0;j<3;j++){//change camera position
+			for (int k = 0;k<2;k++){//draw with and without object movement
+				float rotY = 0;
+				glm::vec3 scale = glm::vec3(1, 1, 1);
+				glm::vec3 geopos = glm::vec3(0, 0, 0);
+				Geometry * geo = geos[i];
+				glm::vec4 camPosition = camPos[j];
+
+				glm::vec4 camDir = -camPosition;
+				if (k == 1){
+					scale = glm::vec3(1.5f, 1, 1);
+					geopos = glm::vec3(0, 1.0f, 0);
+					rotY = 45;
+				}
+
+				//TODO build raytracer with the above parameters and execute everything else 
+			}
+		}
 	}
 	
 }
